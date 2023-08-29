7310272dd99cd79afa5a1f6f487da92e9c621f0e
RomarioNijim <romario@almond-qms.com>
2023-08-29 15:19:28 +0300

    crud tests

--- automation/api/tests/petstore/PetCrudTests.spec.ts
+++ automation/api/tests/petstore/PetCrudTests.spec.ts
@@ -3,7 +3,9 @@ import { EndPoints } from "../../../common/navigationEnum/EndPoints";
 import { StatusCode } from "../../../helpers/apiStatusCodes/StatusCodes";
 import Randomizer from "../../../helpers/randomzier/Randomizer";
 import { IPet } from "../../../interfaces/PerInterfaces";
+import path from "path";
 const FormData = require('form-data');
+import * as fs from 'fs';
 
 test.describe('sanity api tests for the pet store api', async () => {
     let randomizer: Randomizer;
@@ -13,6 +15,9 @@ test.describe('sanity api tests for the pet store api', async () => {
     let baseUrl = EndPoints.PET_STORE_BASE_URL;
     let OK: string = 'OK';
     let id: number = 900;
+    let findByStatus: string = 'findByStatus';
+    let uploadImage: string = 'uploadImage';
+    let pet: string = 'pet'
 
     test.beforeEach(async () => {
         randomizer = new Randomizer();
@@ -41,7 +46,7 @@ test.describe('sanity api tests for the pet store api', async () => {
                 ],
                 "status": "available"
             }
-            const response = await request.post(`${baseUrl}/pet`, {
+            const response = await request.post(`${baseUrl}/${pet}`, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -52,30 +57,31 @@ test.describe('sanity api tests for the pet store api', async () => {
         })
     })
 
-    // test('upload an image directly to an existing pet', async ({ request }) => {
-    //     await test.step('upload image for pet', async () => {
-    //         const petId = 511;
-    //         const imageName: string = 'pug.jpeg'
-    //         const imageFilePath = path.join(__dirname, '../../../images', imageName);
-    //         const file = fs.readFileSync(imageFilePath);
-    //         const formData = new FormData();
-    //         formData.append('petId', petId.toString());
-    //         formData.append('file', file, {
-    //             filename: 'pug.jpeg',
-    //             contentType: 'image/jpeg',
-    //         });
-    //         const response = await request.post(`${baseUrl}/pet/${petId}/uploadImage`, {
-    //             headers: {
-    //                 'Content-Type': 'form-data',
-    //             },
-    //             data: formData,
-    //         });
-    //         console.log(await response.body());
-    //         expect(response.status()).toBe(StatusCode.OK);
-    //         expect(response.statusText()).toBe(OK);
-    //     });
-    // })
+    //-------------------------------------------------------------------
+    test.only('upload an image directly to an existing pet', async ({ request }) => {
+        await test.step('upload image for pet', async () => {
+            const imageName: string = 'pug.jpeg'
+            const imageFilePath = path.join(__dirname, '../../../images', imageName);
+            const file = fs.readFileSync(imageFilePath);
+            const formData = new FormData();
+            formData.append('id', id.toString());
+            // formData.append('file', file, {
+            //     filename: 'pug.jpeg',
+            //     contentType: 'image/jpeg',
+            // });
+            const response = await request.post(`${baseUrl}/pet/${id}/uploadImage`, {
+                headers: {
+                    'Content-Type': 'form-data',
+                },
+                data: formData,
+            });
+            console.log(await response.body());
+            expect(response.status()).toBe(StatusCode.OK);
+            expect(response.statusText()).toBe(OK);
+        });
+    })
 
+    //-------------------------------------------------------------------
     test('retrieve pet details via GET request', async ({ request }) => {
         await test.step('fetch the data to validate the data is stored in the server from previous test', async () => {
             const data = {
@@ -96,7 +102,7 @@ test.describe('sanity api tests for the pet store api', async () => {
                 ],
                 "status": "available"
             }
-            const res = await request.get(`${baseUrl}/pet/${id}`, {
+            const res = await request.get(`${baseUrl}/${pet}/${id}`, {
                 headers: {
                     'Content-Type': 'application/json',
                 }
@@ -108,8 +114,8 @@ test.describe('sanity api tests for the pet store api', async () => {
 
         })
     })
-
-    test.only('update an existing pet', async ({ request }) => {
+    //-------------------------------------------------------------------
+    test('update an existing pet', async ({ request }) => {
         const newData = {
             "id": id,
             "category": {
@@ -126,12 +132,10 @@ test.describe('sanity api tests for the pet store api', async () => {
                     "name": `"${randomName}-tag"`
                 }
             ],
-            "status": "not available"
+            "status": "pending"
         }
-
         await test.step('update an existing pet from previous test', async () => {
-
-            const response = await request.put(`${EndPoints.PET_STORE_BASE_URL}/pet`, {
+            const response = await request.put(`${EndPoints.PET_STORE_BASE_URL}/${pet}`, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -141,29 +145,79 @@ test.describe('sanity api tests for the pet store api', async () => {
         })
 
         await test.step('validate the data now matches the new updated data with specific properties', async () => {
-            const res = await request.get(`${EndPoints.PET_STORE_BASE_URL}/pet/${id}`, {
+            const res = await request.get(`${EndPoints.PET_STORE_BASE_URL}/${pet}/${id}`, {
             })
             const responseJson: IPet = await res.json();
             expect([responseJson.id, responseJson.name, responseJson.status]).toEqual([newData.id, newData.name, newData.status]);
         })
     })
 
+    //------------------------------------------------------------------
+
+    test('find pet by pending status #1', async ({ request }) => {
+        await test.step('find a pet by pending status', async () => {
+            const response = await request.get(`${baseUrl}/${pet}/${findByStatus}`, {
+                params: {
+                    status: 'pending'
+                }
+            });
+            expect(response.ok()).toBeTruthy();
+            expect(await response.json()).toContainEqual(expect.objectContaining({
+                "id": 53365508,
+                "name": 'Nemo',
+            }))
+        })
+    })
+
+    //------------------------------------------------------------------
+    test('find pet by status #2', async ({ request }) => {
+        await test.step('find a pet by available status', async () => {
+            const response = await request.get(`${baseUrl}/${pet}/${findByStatus}`, {
+                params: {
+                    status: 'available'
+                }
+            });
+            expect(response.ok()).toBeTruthy();
+            expect(await response.json()).toContainEqual(expect.objectContaining({
+                "name": 'Poki',
+            }))
+        })
+    })
+
+    //------------------------------------------------------------------
+
+    test('find pet by status #3', async ({ request }) => {
+        await test.step('find a pet by sold status', async () => {
+            const response = await request.get(`${baseUrl}/${pet}/${findByStatus}`, {
+                params: {
+                    status: 'sold'
+                }
+            });
+            expect(response.ok()).toBeTruthy();
+            expect(await response.json()).toContainEqual(expect.objectContaining({
+                "name": 'Chappi dog',
+            }))
+        })
+    })
+
+
+    //-------------------------------------------------------------------
 
     test('delete an existing pet', async ({ request }) => {
         await test.step('delete pet with the id from previous test', async () => {
-            const response = await request.delete(`${EndPoints.PET_STORE_BASE_URL}/pet/${id}`, {
+            const response = await request.delete(`${EndPoints.PET_STORE_BASE_URL}/${pet}/${id}`, {
                 headers: {
                     'Content-Type': 'application/json',
-
                 }
             });
             expect(response.status()).toBe(StatusCode.OK);
         })
 
         await test.step('validate there is no such resource with the deleted id anymore', async () => {
-            const res = await request.get(`${baseUrl}/pet/${id}`);
+            const res = await request.get(`${baseUrl}/${pet}/${id}`);
             expect(res.status()).toBe(StatusCode.NOT_FOUND);
         })
     })
+
 })
 
